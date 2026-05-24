729133379731f6728ca4408c7cf69625743e6fb8
jenny <jraffstar@gmail.com>
2026-05-24 19:37:05 +0100

    Basic scaffold implentation
    On MC 1.19.4, the fullbright doesn't work with how it is. Will need to be fixed in a later update

--- cat.lua
+++ cat.lua
@@ -1,10 +1,8 @@
 function main()
     local DateFormatter = luajava.bindClass("java.time.format.DateTimeFormatter")
     local LocalDateTime = luajava.bindClass("java.time.LocalDateTime")
-    
-    local cat = Module.new("Cat", "Yes", "VISUAL", this)
 
-
+    --  HUD --
     this:registerCallback("hud", function(hud)
         -- watermark --
         renderer:textWithShadow(hud:getStack(), "cat §8v0.0.1", vec2d(2, 2), color(255, 255, 255, 255)) 
@@ -44,7 +42,63 @@ function main()
         renderer:textWithShadow(hud:getStack(), text, vec2d(renderer:windowWidth()/2 - renderer:width(text)/2 , 4), color(255, 255, 255, 255))
         
         -- fullbright --
-        mc.options.gamma = 1337
+        -- TODO fix fullbright
+        --mc.options.gamma = 1337
+    end)
+
+    -- Scaffold --
+    local module = Module.new("Scaffold", "", "MOVEMENT", this)
+
+    local placeDelay = 0
+
+    module:body(function()
+        
+
+        local pDelay = NumberBuilder(0):name("PlaceDelay"):setBounds(0, 20):build(module)
+        local autoRotate = BooleanBuilder(true):name("AutoRotate"):build(module)
+        local downCount = NumberBuilder(1):name("DownCount"):setBounds(1, 5):build(module)
+        local swing = BooleanBuilder(true):name("Swing"):build(module)
+
+        module:registerCallback("events", function(event)
+
+            if(event:getName() == "tick") then
+
+                placeDelay = placeDelay + 1
+
+                -- Place blocks below feet
+                if(placeDelay >= pDelay:getValue()) then
+                    for i = 1, downCount:getValue() do
+                        local x = math.floor(mc.player:getX())
+                        local y = math.floor(mc.player:getY()) - i
+                        local z = math.floor(mc.player:getZ())
+
+                        local blockName = tostring(globals:getBlock(x, y, z))
+                        
+                        -- Check if block is air (replaceable)
+                        if(blockName == "Block{minecraft:air}") then
+                            local pos = luajava.newInstance("net.minecraft.util.math.BlockPos", x, y, z)
+                            
+                            if(interactions:isPlaceable(pos, autoRotate:getValue())) then
+
+                                if(swing:getValue()) then
+                                    local Hand = luajava.bindClass("net.minecraft.util.Hand")
+                                    mc.player:swingHand(Hand.MAIN_HAND)
+                                end
+                                interactions:place(pos, autoRotate:getValue())
+                                placeDelay = 0
+                                return
+                            end
+                        end
+                    end
+                end
+
+            end
+
+        end)
+
+        module:onEnable(function()
+            placeDelay = pDelay:getValue()
+        end)
 
     end)
 end
